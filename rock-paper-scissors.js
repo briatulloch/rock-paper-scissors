0ff04b4312f521d5fc76d5e539c1564a40f3b63b
Bria <mynameisbria@hotmail.com>
2021-04-08 15:28:32 -0400

    add reset button

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -9,6 +9,7 @@ const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
+const reset=document.getElementById("reset");
 
 //Gets random selection from computer 
 function getComputerSelection() {
@@ -29,7 +30,7 @@ function win(playerSelection, computerSelection) {
     compScore_span.innerHTML = compScore;
 
     if (userScore < 5){result_p.innerHTML = `${convertToWord(playerSelection)} beats ${convertToWord(computerSelection)}. You win! =D`;
-    }else if(userScore===5){result_p.innerHTML='Game over, you win! Refresh to play again';
+    }else if(userScore===5){result_p.innerHTML='Game over, you win! Click reset to play again';
     rock_div.removeEventListener('click', handleRock);
     paper_div.removeEventListener('click', handlePaper);
     scissors_div.removeEventListener('click', handleScissors);  
@@ -44,7 +45,7 @@ function lose(playerSelection, computerSelection) {
     compScore_span.innerHTML = compScore;
     
     if (compScore<5){result_p.innerHTML = `${convertToWord(computerSelection)} beats ${convertToWord(playerSelection)}. You lose =(`;
-    }else if(compScore===5){result_p.innerHTML='Game over, you lose! Refresh to play again';
+    }else if(compScore===5){result_p.innerHTML='Game over, you lose! Click reset to play again';
     rock_div.removeEventListener('click', handleRock);
     paper_div.removeEventListener('click', handlePaper);
     scissors_div.removeEventListener('click', handleScissors);  
@@ -86,9 +87,25 @@ function game(playerSelection) {
       game("s");
     }
 
+    function resetGame(){
+      userScore=0;
+      compScore=0;
+      userScore_span.innerHTML = "0";
+      compScore_span.innerHTML = "0";
+      result_p.innerHTML="PAPER COVERS ROCK. YOU WIN!"  
+      rock_div.addEventListener('click', handleRock);
+ 
+      paper_div.addEventListener('click', handlePaper);
+      
+      scissors_div.addEventListener('click', handleScissors); 
+    };
+
   //Links images to game play  
  rock_div.addEventListener('click', handleRock);
  
  paper_div.addEventListener('click', handlePaper);
  
  scissors_div.addEventListener('click', handleScissors);
+
+ //reset game
+ reset.addEventListener('click', resetGame);
